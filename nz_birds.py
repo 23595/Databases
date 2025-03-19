1a0c88f6592390367273daa4eef0c173b2254c1f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-03-20 11:44:50 +1300

    Cleaned up code, added comments

--- nz_birds.py
+++ nz_birds.py
@@ -1,8 +1,10 @@
+'''Code to access information from a database of nz birds'''
 import sqlite3
 
 #Variable declarations
 DATABASE = 'nz_birds.db'
 user_input = "1"
+#Define the queries in variables
 statement1 = 'SELECT * FROM Birds;'
 statement2 = 'SELECT common_name FROM Birds ORDER BY common_name;'
 statement3 = 'SELECT scientific_name, common_name FROM Birds ORDER BY scientific_name;'
@@ -10,20 +12,30 @@ statement4 = 'SELECT common_name, max_run_speed FROM Birds ORDER BY max_run_spee
 statement5 = 'SELECT common_name, status FROM Birds;'
 statement6 = "SELECT common_name, status FROM Birds WHERE status = 'Endangered';"
 
-#Define functions
-def print_information(statement):
+#Define a function
+def print_information(statement): #Prints the info with a given statement
     '''Prints information using the SQL statement entered'''
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        sql = statement
-        cursor.execute(sql)
+        cursor.execute(statement)
         results = cursor.fetchall()
         #print the results
         for bird in results:
             print(bird)
 
 while True:
-    user_input = input("\nWhat would you like to see? Input a number.\n1.All data\n2.Common names\n3.Common & Scientific names\n4.Run speeds\n5.Status\n6.Endangered birds\n7.Exit\n")
+    #Ask for input
+    user_input = input('''
+What would you like to see? Input a number.
+    1.All data
+    2.Common names
+    3.Common & Scientific names
+    4.Run speeds
+    5.Status
+    6.Endangered birds
+    7.Exit
+''')
+    #Print info depending on input
     if user_input == "1":
         print_information(statement1)
     elif user_input == "2":
